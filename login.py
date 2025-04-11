aa77ac1f9dfa0b8ce5dd630b0a5cc487eb6f851e
gayathri7022 <gayathria7022@gmail.com>
2025-04-12 02:37:22 +0530

    Adding modified files

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 dic = {"Arun": "Arun@134", "Navya":"Navya@37", "Asha":"Asha8838"}
 
-def log(name,pas):
+def log(name, pas):
     if name in dic:
         if len(pas) == 8:
             if pas == dic[name]:
@@ -14,8 +14,8 @@ def log(name,pas):
     else:
         return "Invalid user"
 
-log("Arun","Arun@134")
-log("hdjds","Arun@134")
+log("Arun", "Arun@134")
+log("hdjds", "Arun@134")
 log("Navya", "27362872")
 log("Asha", "grfijkjh")
 
